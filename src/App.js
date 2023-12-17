8330401fc719ddfe0795f5395b567306260d1b16
HaddeeAndd <haddeeann@gmail.com>
2023-12-17 12:11:28 -0500

    let's let it go, im not able to do the animations that I would like.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import Courses from './pages/Courses';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
 import { createClient } from '@supabase/supabase-js'
-import {useEffect} from "react";
+import React from "react";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.REACT_APP_NEXT_PUBLIC_SUPABASE_URL, process.env.REACT_APP_NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
 function App() {
-    useEffect(() => {
-        async function fetchData () {
-            const { data, error } = await supabase
-                .from('courses')
-                .select()
-            return data
-        }
-
-        fetchData().then(data => {
-            console.log(data)
-        })
-    }, [])
-
   return (
     <div>
         <Header />
-        <Courses />
+        <Courses supabase={supabase} />
         <Footer />
     </div>
   );
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,7 +1,38 @@
-function Courses() {
+import React, { useEffect, useState } from "react";
+import './Courses.css';
+
+function Courses({ supabase }) {
+    const [courseList, setCourseList] = useState([])
+    useEffect(() => {
+        async function fetchData () {
+            const { data, error } = await supabase
+                .from('courses')
+                .select()
+            return data
+        }
+
+        fetchData().then(data => {
+            setCourseList(data)
+        })
+    }, [])
     return (
         <div>
-            Course list and progress
+            {courseList.map(course => (
+                <a
+                    key={course.id}
+                    href={course.url || "#"} // Use "#" as fallback for courses without a URL
+                    className={`course-card ${course.url ? "hover-effect" : ""}`}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <h2>{course.title}</h2>
+                    <p><strong>Description:</strong> {course.description}</p>
+                    <p><strong>Duration:</strong> {course.duration}</p>
+                    <p><strong>URL:</strong> {course.url}</p>
+                    <p><strong>Creator:</strong> {course.creator}</p>
+                    <p><strong>Current Progress:</strong> {course.current_progress}</p>
+                </a>
+            ))}
         </div>
     );
 }
