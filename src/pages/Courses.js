ea2b716774de7d1c49bc7a3e1c1ead9a80dd5936
HaddeeAndd <haddeeann@gmail.com>
2023-12-17 22:01:58 -0500

    update time on the course tracking parts

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import './Courses.css';
 
 function Courses({ supabase }) {
-    const [courseList, setCourseList] = useState([])
+    const [courseList, setCourseList] = useState([]);
+    const [timeInput, setTimeInput] = useState('');
     useEffect(() => {
         async function fetchData () {
             const { data, error } = await supabase
@@ -18,20 +19,36 @@ function Courses({ supabase }) {
     return (
         <div>
             {courseList.map(course => (
-                <a
-                    key={course.id}
-                    href={course.url || "#"} // Use "#" as fallback for courses without a URL
-                    className={`course-card ${course.url ? "hover-effect" : ""}`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
+                <div>
                     <h2>{course.title}</h2>
                     <p><strong>Description:</strong> {course.description}</p>
                     <p><strong>Duration:</strong> {course.duration}</p>
-                    <p><strong>URL:</strong> {course.url}</p>
                     <p><strong>Creator:</strong> {course.creator}</p>
                     <p><strong>Current Progress:</strong> {course.current_progress}</p>
-                </a>
+                    <a
+                        key={course.id}
+                        href={course.url || "#"} // Use "#" as fallback for courses without a URL
+                        className='course-link'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        {course.url}
+                    </a>
+                    <button>Update Time</button>
+                    <input
+                        value={timeInput}
+                        onChange={e => setTimeInput(e.target.value)}
+                    />
+                    {course.url && course.url.includes('youtube') &&
+                        <div>
+                            <iframe width="560" height="315"
+                                    src="https://www.youtube.com/embed/ua-CiDNNj30?si=ZF7CRhth9FDfwHW5&amp;start=3600"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowFullScreen></iframe>
+                        </div>
+                    }
+                </div>
             ))}
         </div>
     );
