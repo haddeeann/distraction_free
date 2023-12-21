d76db5e4e3ecbb6fe113db2f0534383b3c310e3c
HaddeeAndd <haddeeann@gmail.com>
2023-12-20 21:19:41 -0500

    add time set up for getitn gtime from the data base

--- src/pages/CourseVideo.js
+++ src/pages/CourseVideo.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-function CourseVideo({ videoId }) {
+function CourseVideo({ course }) {
+    const [hourInput, setHourInput] = useState(undefined);
+    const [minuteInput, setMinuteInput] = useState(undefined);
+    const [secondInput, setSecondInput] = useState(undefined);
+    const [totalSeconds, setTotalSeconds] = useState(0)
+
     useEffect(() => {
         let player; // Reference to the YouTube player
-
+        console.log(course)
+        const videoId = course.video_id
+        const startTime = course.stopping_point
+        const courseUrl = "https://www.youtube.com/embed/ua-CiDNNj30?si=zrbI_Hxa_4BSpM_7&amp;start=6892"
         // Function to initialize the YouTube player
         function initPlayer() {
             player = new window.YT.Player("youtube-player", {
                 videoId: videoId,
                 playerVars: {
-                    start: '3600',
+                    start: {startTime},
                 },
                 events: {
                     onReady: onPlayerReady,
@@ -46,9 +54,57 @@ function CourseVideo({ videoId }) {
                 player.destroy();
             }
         };
-    }, [videoId]);
+    }, []);
 
-    return <div id="youtube-player"></div>;
+    const updateTime = () => {
+        let hours = 0
+        let minutes = 0
+        let seconds = 0
+        if (hourInput) {
+            hours = Number(hourInput) * 60 * 60
+        }
+        if (minuteInput) {
+            minutes = Number(minuteInput) * 60
+        }
+        if (secondInput) {
+            seconds = Number(secondInput)
+        }
+        const t = hours + minutes + seconds;
+        if (hours || minutes || seconds) {
+            setTotalSeconds(t)
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <span>hours: </span>
+                    <input
+                        value={hourInput}
+                        onChange={e => setHourInput(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>minutes: </span>
+                    <input
+                        value={minuteInput}
+                        onChange={e => setMinuteInput(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>seconds: </span>
+                    <input
+                        value={secondInput}
+                        onChange={e => setSecondInput(e.target.value)}
+                    />
+                </div>
+
+                <button onClick={updateTime}>Update Times</button>
+            </div>
+            <div id="youtube-player"></div>
+        </div>
+    );
 }
 
 export default CourseVideo;
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -4,7 +4,6 @@ import CourseVideo from './CourseVideo'
 
 function Courses({ supabase }) {
     const [courseList, setCourseList] = useState([]);
-    const [timeInput, setTimeInput] = useState('');
     useEffect(() => {
         async function fetchData () {
             const { data, error } = await supabase
@@ -21,17 +20,6 @@ function Courses({ supabase }) {
         })
     }, [])
 
-    function createIFrame(course) {
-        const videoId = 'ua-CiDNNj30?si=ZF7CRhth9FDfwHW5'
-        const start = '3600'
-        return (
-            <iframe width="560" height="315"
-                    src={`https://www.youtube.com/embed/${videoId}&amp;start=${start}`}
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-        )
-    }
     return (
         <div>
             {courseList.map(course => (
@@ -41,14 +29,7 @@ function Courses({ supabase }) {
                     <p><strong>Duration:</strong> {course.duration}</p>
                     <p><strong>Creator:</strong> {course.creator}</p>
                     <p><strong>Current Progress:</strong> {course.current_progress}</p>
-                    <div>
-                        <button>Update Times</button>
-                        <input
-                            value={timeInput}
-                            onChange={e => setTimeInput(e.target.value)}
-                        />
-                    </div>
-                    <CourseVideo videoId='ua-CiDNNj30?si=ZF7CRhth9FDfwHW5' />
+                    <CourseVideo course={course} />
                 </div>
             ))}
         </div>
