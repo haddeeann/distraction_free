1089ebfddffb06d30f1a6d13befc99eca355f365
HaddeeAndd <haddeeann@gmail.com>
2023-12-23 12:00:14 -0500

    add a bit of padding to the courses list and add a red border to all but last of courses.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,27 @@
 import mushroom_logo from '../images/mushroom_logo.jpg';
 import '../css/Header.css';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header>
-            <div id="logo-container">
-                <div className="logo">
-                    <a href="/">
-                        <img src={mushroom_logo} width='125px' height='auto' alt="Distraction Free" />
-                    </a>
+            <div id='logo-container'>
+                <div className='logo'>
+                    <Link to='/'>
+                        <img src={mushroom_logo} width='125px' height='auto' alt='Distraction Free' />
+                    </Link>
                 </div>
-                <div className="logo-text-small">
-                    <a href="/">Distraction Free: Data Science Videos</a>
+                <div className='logo-text-small'>
+                    <Link to='/'>
+                        <div>Distraction Free</div>
+                        <div>Data Science Videos</div>
+                    </Link>
                 </div>
             </div>
-            {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">General Learning</Link>
-                    </li>
-                    <li>
-                        <Link to="/stats">Statistics</Link>
-                    </li>
-                    <li>
-                        <Link to="/python">Python</Link>
-                    </li>
-                </ul>
+            <nav id='nav-container'>
+                <div><Link to='/'>General Learning</Link></div>
+                <div><Link to='/stats'>Statistics</Link></div>
+                <div><Link to='/python'>Python</Link></div>
             </nav>
         </header>
     );
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -45,7 +45,7 @@ function Courses({ topic }) {
         <div className='courses'>
             The list on YouTube <a href='https://www.youtube.com/watch?v=ua-CiDNNj30&list=PLWKjhJtqVAblQe2CCWqV4Zy3LY01Z8aF1' target='_blank' rel='noreferrer'>Data Science List</a>
             {courseList.map(course => (
-                <div key={course.id}>
+                <div key={course.id} class='course'>
                     <h2>{course.title}</h2>
                     <p><strong>Description:</strong> {course.description}</p>
                     <p><strong>Duration:</strong> {course.duration}</p>
@@ -54,7 +54,6 @@ function Courses({ topic }) {
                             title="YouTube video player" frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowFullScreen></iframe>
-                    <hr />
                 </div>
             ))}
         </div>
