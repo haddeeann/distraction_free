1d8c08b66f3cb7b501c715ced1a7978dbe158a05
HaddeeAndd <haddeeann@gmail.com>
2023-12-23 10:25:09 -0500

    home and other pages have footer and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import mushroom_logo from '../images/mushroom_logo.jpg';
 import './Header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -14,6 +15,24 @@ function Header() {
                     <a href="/">Distraction Free: Data Science Videos</a>
                 </div>
             </div>
+            {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/nothing-here">Nothing Here</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
